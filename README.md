f9657f87fe45ef80a9744fc341559ae1ac16a6f4
abduqodir <106659894+abdudoqir@users.noreply.github.com>
2023-11-15 21:10:50 +0500

    birnima

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # shop
- shop
+
+## shop website 2023-11-15
+
+Bu portfolio uchun website hisoblanadi
+
+## Installation
+
+```
+pip install -r requirements.txt
+```
+
+## Usage
+
+```
+python manage.py runserver
+```
+
+# TELEGRAM
+[Telegram nick](https://t.me/xackerkhon)
+
+## Contributing
+
+Pull requestlar qabul qilinadi. Katta o'zgarishlardan oldin o'zingizga branch yarating.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+## Project Status
+
+Dastur hali ishlanmadi.
+
+## Project Screen Shot(s)
+
+![](D:\fon\4cb6bcbb863017f2f702e5fe07becaf1.jpg)
--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+urlpatterns += i18n_patterns(
+    path('user/', include('user.urls')),
+    path('blog/', include('blog.urls')),
+    path('order/', include('order.urls')),
+    path('product/', include('product.urls')),
+    prefix_default_language=False,
+)
+
+# static and media files
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
